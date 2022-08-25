d9fac40da69fe2b7fd8295ea5893b8f07c8b0314
Jonathan Trost <Jonathan Trost>
2022-08-24 21:07:34 -0400

    Begain planning with collision decteciton and rounded rectangles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "Platform.h"
 #include "../include/raylib.h"
 
+#define MOUSE_SCALE_MARK_SIZE 12
+
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
@@ -10,6 +12,13 @@ int main(void) {
     Vector2 ballPosition = { 100.0f, 100.0f };
     Color ballColor = DARKBLUE;
 
+    Rectangle rec = {100, 100, 200, 80};
+    Vector2 mousePosition = { 0 };
+
+    bool mouseScaleReady = false;
+    bool mouseScaleMode = false;
+    bool makeCircle = false;
+
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
@@ -17,6 +26,7 @@ int main(void) {
         ballPosition = GetMousePosition();
         // Update Input
         // ----------------------------------------------
+        
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ballColor = MAROON;
         else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
         else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
@@ -24,6 +34,39 @@ int main(void) {
         else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
         else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
         else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
+        
+        // ----------------------------------------------
+        mousePosition = GetMousePosition();
+
+        if (CheckCollisionPointRec(mousePosition, rec) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            makeCircle = true;
+        }
+        else makeCircle = false;
+
+        if (CheckCollisionPointRec(mousePosition, (Rectangle) {rec.x + rec.width - MOUSE_SCALE_MARK_SIZE, 
+                                                                rec.y + rec.height - MOUSE_SCALE_MARK_SIZE,
+                                                               MOUSE_SCALE_MARK_SIZE, MOUSE_SCALE_MARK_SIZE })) {
+            mouseScaleReady = true;
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) mouseScaleMode = true;
+        }
+        else mouseScaleReady = false;
+
+        if (mouseScaleMode) {
+            mouseScaleReady = true;
+            
+            rec.width = (mousePosition.x - rec.x);
+            rec.height = (mousePosition.y - rec.y);
+            
+            // Check Minimum Rec Size
+            if (rec.width < MOUSE_SCALE_MARK_SIZE) rec.width = MOUSE_SCALE_MARK_SIZE;
+            if (rec.height < MOUSE_SCALE_MARK_SIZE) rec.height = MOUSE_SCALE_MARK_SIZE;
+
+            // Check Max rec size
+            if (rec.width > (GetScreenWidth() - rec.x)) rec.width = GetScreenWidth() - rec.x;
+            if (rec.height > (GetScreenHeight() - rec.y)) rec.height = GetScreenHeight() - rec.y;
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) mouseScaleMode = false;
+        }
         // Draw
         // ----------------------------------------------
         BeginDrawing();
@@ -31,6 +74,19 @@ int main(void) {
             ClearBackground(RAYWHITE);
 
             DrawCircleV(ballPosition, 40, ballColor);
+            DrawRectangleRounded(rec, 0.5f, 30, Fade(GREEN, 0.5f));
+
+            if (makeCircle) {
+                DrawCircleGradient((int)ballPosition.x, (int)ballPosition.y, 60.0f, RED, ORANGE);
+            }
+
+            if (mouseScaleReady) {
+                DrawRectangleLinesEx(rec, 1, RED);
+                DrawTriangle((Vector2) { rec.x + rec.width - MOUSE_SCALE_MARK_SIZE, rec.y + rec.height},
+                             (Vector2) { rec.x + rec.width, rec.y + rec.height }, 
+                             (Vector2) { rec.x + rec.width, rec.y + rec. height - MOUSE_SCALE_MARK_SIZE},
+                             RED);
+            }
 
             DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
 
