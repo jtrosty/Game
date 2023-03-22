be3a074c8561e363f28a44ed36650960039c3407
Jon Trost <j.trosty@gmail.com>
2023-03-22 16:06:48 -0400

    push to remote distro to work on macbook while away.

--- space/src/main.zig
+++ space/src/main.zig
@@ -7,51 +7,55 @@
 
 const std = @import("std");
 const rl = @import("raylib");
+const rlm = @import("raylib-math");
 
 const Player = struct {
-    shipHeight:     f32;
-    position:       rl.Vector2;
-    speed:          rl.Vector2;
-    acceleartion:   f32;
-    rotation:       f32;
-    collider:       rl.Vector3;
-    color:          rl.Color;
+    shipHeight: f32,
+    position: rl.Vector2,
+    speed: rl.Vector2,
+    acceleartion: f32,
+    rotation: f32,
+    collider: rl.Vector3,
+    color: rl.Color,
 };
 
 const Bullet = struct {
-    shipHeight: f32;
- 
+    shipHeight: f32,
 };
 
 const Particle = struct {
-    rl.Vector2 position;
-    rl.Color color;
-    life:
+    position: rl.Vector2,
+    color: rl.Color,
+    //life,
     // Particle type
 };
 
-
-pub fn DrawParticle(&p: Particle) {
+pub fn DrawParticle(p: *Particle) void {
     rl.DrawCircle(p.position.x, p.position.y, 5, p.color);
 }
 
+const PI: f32 = 3.141592653589;
+const DEG2RAD: f32 = PI / 180.0;
 
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 800;
     const screenHeight = 450;
+    const SHIP_HEIGHT = 10.0;
 
-    const player: Player{
-        shipHeight = 5.0;
-        position.x = screenWidth / 2;
-        position.y = screenHeight / 2;
-        acceleartion = 0.0;
-        rotation = 0.0;
-        collider.x = player.position.x + @sin(player.rotation * rl.DEG2RAD) * player.shipHeight / 2.5f;
-        collider.y = player.position.y - @cos(player.rotation * rl.DEG2RAD) * player.shipHeight / 2.5f;
-        collider.z = 12;
-        color = rl.RED;
+    var player = Player{
+        .shipHeight = SHIP_HEIGHT,
+        .position = rl.Vector2{ .x = screenWidth / 2.0, .y = screenHeight / 2.0 },
+        .acceleartion = 0.0,
+        .speed = rl.Vector2{ .x = 0.0, .y = 0.0 },
+        .rotation = 0.0,
+        .collider = rl.Vector3{
+            .x = (screenWidth / 2) + @sin(0.0 * DEG2RAD) * (SHIP_HEIGHT / 2.5),
+            .y = (screenHeight / 2) - @cos(0.0 * DEG2RAD) * (SHIP_HEIGHT / 2.5),
+            .z = 12,
+        },
+        .color = rl.RED,
     };
 
     rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
@@ -74,9 +78,11 @@ pub fn main() anyerror!void {
         // Draw buttons: basic
         if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_MIDDLE_RIGHT)) {}
         if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_MIDDLE_LEFT)) {}
-        if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_LEFT)) {}
+        if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_LEFT)) {
+            player.color = rl.YELLOW;
+        }
         if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_DOWN)) {
-            playerColor = rl.BLUE;
+            player.color = rl.BLUE;
         }
         if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_RIGHT)) {}
         if (rl.IsGamepadButtonDown(0, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_UP)) {}
@@ -93,18 +99,21 @@ pub fn main() anyerror!void {
         var debug_gamepadAxisLY: f32 = rl.GetGamepadAxisMovement(0, 1) * movementMultiplier;
         var debug_gamepadAxisRX: f32 = rl.GetGamepadAxisMovement(0, 2) * movementMultiplier;
         var debug_gamepadAxisRY: f32 = rl.GetGamepadAxisMovement(0, 3) * movementMultiplier;
-        player.position.x += @floatToInt(i32, debug_gamepadAxisLX);
-        player.position.y += @floatToInt(i32, debug_gamepadAxisLY);
+        player.position.x += debug_gamepadAxisLX;
+        player.position.y += debug_gamepadAxisLY;
+        const zeroPos = rl.Vector2{ .x = 0, .y = 0 };
+        const dirVect = rl.Vector2{ .x = debug_gamepadAxisLX, .y = debug_gamepadAxisLY };
+        player.rotation = rlm.Vector2Angle(zeroPos, dirVect);
         debug_gamepadAxisLX = 1.0;
 
         //----------------------------------------------------------------------------------
         rl.BeginDrawing();
 
         rl.ClearBackground(rl.WHITE);
-        rl.DrawCircle(playerX, playerY, 9, rl.RED);
-        const v1: rl.Vector2 = { player.position.x + @sinf(player.rotation*DEG2RAD)*(player.shipHeight), player.position.y - @cosf(player.rotation*DEG2RAD)*(player.shipHeight) };
-        const v2: rl.Vector2 = { player.position.x - @cosf(player.rotation*DEG2RAD)*(PLAYER_BASE_SIZE/2), player.position.y - @sinf(player.rotation*DEG2RAD)*(player.shipHeight/2) };
-        const v3: rl.Vector2 = { player.position.x + @cosf(player.rotation*DEG2RAD)*(PLAYER_BASE_SIZE/2), player.position.y + @sinf(player.rotation*DEG2RAD)*(player.shipHeight/2) };
+        //rl.DrawCircleV(player.position, 9, rl.RED);
+        const v1 = rl.Vector2{ .x = player.position.x + @sin(player.rotation * DEG2RAD) * (player.shipHeight), .y = player.position.y - @cos(player.rotation * DEG2RAD) * (player.shipHeight) };
+        const v2 = rl.Vector2{ .x = player.position.x - @cos(player.rotation * DEG2RAD) * (player.shipHeight / 2), .y = player.position.y - @sin(player.rotation * DEG2RAD) * (player.shipHeight / 2) };
+        const v3 = rl.Vector2{ .x = player.position.x + @cos(player.rotation * DEG2RAD) * (player.shipHeight / 2), .y = player.position.y + @sin(player.rotation * DEG2RAD) * (player.shipHeight / 2) };
         rl.DrawTriangle(v1, v2, v3, player.color);
         rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LIGHTGRAY);
 
@@ -117,7 +126,6 @@ pub fn main() anyerror!void {
         rl.DrawText(rl.TextFormat("LY AXIS %i: %.2f", debug_axis[1], debug_gamepadAxisLY), 20, 70 + 20 * 1, 10, rl.DARKGRAY);
         rl.DrawText(rl.TextFormat("RX AXIS %i: %.2f", debug_axis[2], debug_gamepadAxisRX), 20, 70 + 20 * 2, 10, rl.DARKGRAY);
         rl.DrawText(rl.TextFormat("RY AXIS %i: %.2f", debug_axis[3], debug_gamepadAxisRY), 20, 70 + 20 * 3, 10, rl.DARKGRAY);
-        rl.DrawText(rl.TextFormat("test %.2f", testVar), 20, 70 + 20 * 4, 10, rl.DARKGRAY);
         //std.debug.print("Left Xaxis: {d}\n", .{debug_gamePadAxisLX});
         //
         //        for (debug_axis) |i| {
