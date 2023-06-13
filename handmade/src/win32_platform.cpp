5d95e0f385e75033335948f4e555fa1ccab2c881
Jon Trost <j.trosty@gmail.com>
2023-06-13 08:58:31 -0400

    Worked on windows platform layer.

--- handmade/src/win32_platform.cpp
+++ handmade/src/win32_platform.cpp
@@ -1,55 +1,86 @@
+// Not needed because i"m running with windows A funcitons
+/*
 #ifndef UNICODE
 #define UNICODE
 #endif 
+*/
 
 #include <windows.h>
+#include "core.h"
+
+struct Render_Buffer {
+    int width;
+    int height;
+    u32* pixels;
+    BITMAPINFO bitmap_info;
+};
+
+global_variable Render_Buffer render_buffer;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     // Register the window class.
-    const wchar_t CLASS_NAME[]  = L"Core Game Trost";
+    //const wchar_t CLASS_NAME[]  = L"Core Game Trost";
+    char RUNNING = 1;
     
-    WNDCLASS wc = { };
+    WNDCLASSA window_class = {};
+    window_class.style = CS_HREDRAW|CS_VREDRAW;
+    window_class.lpfnWndProc   = WindowProc;
+    window_class.hInstance = hInstance;
+    window_class.lpszClassName = "Core_Window_Class";
+    // window_class.lpfnWndProc = Win32MainWindowCallback;
+    // WindowClass.hIcon;
 
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    RegisterClassA(&window_class);
 
-    RegisterClass(&wc);
+    //Win32ResizeDIBSection(&GlobalBackbuffer, 1280, 720);
+    
+    const int INITIAL_WINDOW_X = 1280;
+    const int INITIAL_WINDOW_Y = 720;
 
-    // Create the window.
-
-    HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L"Trost Core GAme",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
-
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
+    HWND window = CreateWindowExA(
+        0,                                  // Optional window styles.
+        window_class.lpszClassName,         // Window class
+        "Trost Core Game",                  // Window text
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,   // Window style
+        CW_USEDEFAULT, CW_USEDEFAULT,       // Position
+        INITIAL_WINDOW_X, INITIAL_WINDOW_Y, // Size
+        NULL,                               // Parent window    
+        NULL,                               // Menu
+        hInstance,                          // Instance handle
+        NULL                                // Additional application data
         );
 
-    if (hwnd == NULL)
-    {
+    if (window == NULL) {
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    HDC hdc = GetDC(window);
+
+    // TODO: ??
+    ShowWindow(window, nCmdShow);
 
     // Run the message loop.
+    while (RUNNING) {
+        MSG msg = { };
+        while (GetMessage(&msg, NULL, 0, 0) > 0)
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
 
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        StretchDIBits(
+            hdc,                                // hdc,
+            0, 0,                               // x and y dest,
+            INITIAL_WINDOW_X, INITIAL_WINDOW_Y, // Destwidht and height,
+            0, 0,                               // x and y src,
+            INITIAL_WINDOW_X, INITIAL_WINDOW_Y, // src width and hegith,
+            const BITMAPINFO                    // *lpbmi,
+            DIB_RGB_COLORS,                                // iUsage,
+            SRCCOPY                               // rop
+        );
     }
 
     return 0;
